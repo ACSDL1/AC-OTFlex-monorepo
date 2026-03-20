2e0f267c1a52ec1c6bf03a68bf4eb96798bb2fcc
gavintranquilino <gavintranquilino@gmail.com>
2026-03-20 18:40:21 -0400

    Successful Dry Run with Xarm

--- devices/potentiostat/__init__.py
+++
@@ -1,9 +0,0 @@
-from .barebones import SerialPortInfo, SerialPotentiostat, list_candidate_device_paths, list_serial_devices, pyserial_available
-
-__all__ = [
-    'SerialPortInfo',
-    'SerialPotentiostat',
-    'list_candidate_device_paths',
-    'list_serial_devices',
-    'pyserial_available',
-]
--- src/adapters/__init__.py
+++ src/adapters/__init__.py
@@ -1,9 +1,7 @@
 from .pi_cam import capture_pi_image_via_ssh, connect_pi_ssh, rotate_image_if_needed
-from .potentiostat_adapter import PotentiostatAdapter
 
 __all__ = [
 	"connect_pi_ssh",
 	"capture_pi_image_via_ssh",
 	"rotate_image_if_needed",
-	"PotentiostatAdapter",
 ]
